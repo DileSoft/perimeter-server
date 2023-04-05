fd30bd000cbb076c4729b2c41c1b1ab7c714d35b
dilesoft <dilesoft@gmail.com>
2023-04-05 18:05:45 +0300

    perimetr server

---
+++ index.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+
+const app = express();
+
+interface Game {
+    id: string;
+    gameName: string;
+    missionName: string;
+    gameVer: string;
+    numPlayers: number;
+    maxPlayers: number;
+    host: string;
+    port: number;
+    lastUpdate: number;
+}
+
+let games:Game[] = [];
+
+app.post('/new_game', (req, res) => {
+    const game:Game = req.body;
+    game.host = req.ip;
+    game.port = req.socket.remotePort || 0;
+    game.lastUpdate = Date.now();
+    games.push(req.body);
+});
+
+app.post('/update_game', (req, res) => {
+    const update: Game = req.body;
+    const game = games.find(g => g.id === update.id);
+    if (game) {
+        game.gameName = update.gameName;
+        game.missionName = update.missionName;
+        game.gameVer = update.gameVer;
+        game.numPlayers = update.numPlayers;
+        game.maxPlayers = update.maxPlayers;
+        game.lastUpdate = Date.now();
+    }
+});
+
+app.get('/list_games', (req, res) => {
+    res.send(games);
+});
+
+app.listen(3002, () => {
+    console.log('Server is running on port 3002');
+});
+
+setInterval(() => {
+    const now = Date.now();
+    games = games.filter(g => now - g.lastUpdate < 10 * 60 * 1000);
+}, 10000);
